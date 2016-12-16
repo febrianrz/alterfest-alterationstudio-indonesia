222586e4989945214da8213833a5b4ceead2c1d7
Febrian <febrianrz@gmail.com>
2016-12-16 09:45:11 +0700

    Create activty

---
+++ app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/RegisterActivity.java
@@ -0,0 +1,13 @@
+package id.web.alterationstudio.sewainaja.alterfest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
