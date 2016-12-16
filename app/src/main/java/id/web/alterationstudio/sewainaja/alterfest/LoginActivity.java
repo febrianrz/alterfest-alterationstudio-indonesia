49b7b6ddce88d686ca0a8ae9f7565483d223b400
Febrian <febrianrz@gmail.com>
2016-12-16 09:35:02 +0700

    membuat tampilan login

--- app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/LoginActivity.java
+++ app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/LoginActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * Membuat Halaman Login
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
