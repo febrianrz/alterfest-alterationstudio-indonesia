44b24ac4685569ca907ffc6aa445788ccb638a45
Febrian <febrianrz@gmail.com>
2016-12-16 09:39:56 +0700

    Mendesain tampilan login menggunakan design editor

--- app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/LoginActivity.java
+++ app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/LoginActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * Tampilan Login Awal
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
