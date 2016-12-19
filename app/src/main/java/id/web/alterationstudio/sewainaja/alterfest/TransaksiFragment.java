5746222e37ed38ac27819613613c3904daff4442
Febrian <febrianrz@gmail.com>
2016-12-19 20:02:20 +0700

    design transaksi fragment

---
+++ app/src/main/java/id/web/alterationstudio/sewainaja/alterfest/TransaksiFragment.java
@@ -0,0 +1,29 @@
+package id.web.alterationstudio.sewainaja.alterfest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TransaksiFragment extends Fragment {
+
+
+    public TransaksiFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaksi, container, false);
+    }
+
+}
